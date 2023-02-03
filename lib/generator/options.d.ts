d3b77cddcf1e86610c6081e9a528764ff9d3261c
Jonatan Orozco <jonatan.orozco@kiube.co>
2023-02-03 16:51:25 -0500

    fix: included lib

---
+++ lib/generator/options.d.ts
@@ -0,0 +1,24 @@
+import { EmitBlockKind } from "./emit-block";
+export interface ExternalGeneratorOptions {
+    emitDMMF?: boolean;
+    emitTranspiledCode?: boolean;
+    simpleResolvers?: boolean;
+    useOriginalMapping?: boolean;
+    useUncheckedScalarInputs?: boolean;
+    emitIdAsIDType?: boolean;
+    emitOnly?: EmitBlockKind[];
+    emitRedundantTypesInfo?: boolean;
+    customPrismaImportPath?: string;
+    contextPrismaKey?: string;
+    useSimpleInputs?: boolean;
+}
+export interface InternalGeneratorOptions {
+    outputDirPath: string;
+    prismaClientPath: string;
+}
+export interface GeneratorOptions extends Omit<ExternalGeneratorOptions, "emitOnly" | "contextPrismaKey">, InternalGeneratorOptions {
+    blocksToEmit: EmitBlockKind[];
+    contextPrismaKey: string;
+    relativePrismaOutputPath: string;
+    absolutePrismaOutputPath: string | undefined;
+}
